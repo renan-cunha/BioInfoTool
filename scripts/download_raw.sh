1728d7cd33e16370fe0658676662fe5784fed26e
renan-cunha <renancunhafonseca@gmail.com>
2021-04-13 21:14:19 -0300

    Add script

---
+++ scripts/download_raw.sh
@@ -0,0 +1,10 @@
+curl -o data/blast_data/raw_data/cds_from_genomic.fna.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_cds_from_genomic.fna.gz
+curl -o data/blast_data/raw_data/genomic.fna.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz
+curl -o data/blast_data/raw_data/protein.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_protein.faa.gz
+curl -o data/blast_data/raw_data/rna.fna.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_rna.fna.gz
+curl -o data/blast_data/raw_data/rna_from_genomic.fna.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_rna_from_genomic.fna.gz
+curl -o data/blast_data/raw_data/translated_cds.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_translated_cds.faa.gz
+
+
+
+
---
+++ scripts/extract.sh
@@ -0,0 +1,6 @@
+gzip -d data/blast_data/raw_data/cds_from_genomic.fna.gz
+gzip -d data/blast_data/raw_data/genomic.fna.gz
+gzip -d data/blast_data/raw_data/protein.faa.gz
+gzip -d data/blast_data/raw_data/rna.fna.gz
+gzip -d data/blast_data/raw_data/rna_from_genomic.fna.gz
+gzip -d data/blast_data/raw_data/translated_cds.faa.gz
